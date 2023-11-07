a8ac25781864eac714151ef081c2460459e27a83
Shreya Shinde <shreyarshinde@gmail.com>
2023-11-07 17:46:16 -0500

    Api call to the backend

--- paginationApp/src/app/app.component.ts
+++ paginationApp/src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { ApiResponse } from './interface/api-response';
+import { Page } from './interface/page';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +12,20 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'paginationApp';
+  usersState$:Observable<{appState:string, appData?:ApiResponse<Page> , error?: HttpErrorResponse}>;
 
+  constructor(private userServce:UserService){}
+
+  ngOnInit():void{
+   this.usersState$=this.userServce.users$().pipe(
+     map((response:ApiResponse<Page>)=>{
+      console.log(response);
+      return({appState:'APP_LOADED', appData:response})
+      
+     }
+   ),
+   startWith({appState:'APP_LOADING'}),
+   catchError((error:HttpErrorResponse) => of({appState:'APP_ERROR', error}))
+   )
+  }
 }
